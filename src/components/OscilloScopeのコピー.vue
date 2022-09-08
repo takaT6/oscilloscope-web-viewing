74653537ae034c7f8ee1441366ac7d3b06ebd2db
Cooper <t.taichi6@icloud.com>
2022-09-08 09:27:30 +0900

    hoge

---
+++ src/components/OscilloScopeのコピー.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="teleport-component"/>
+  <div class="controller">
+    <WSController/>
+  </div>
+  <div class="chart">
+    oscilloscope
+    <BarChart />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BarChart from './BarChart.vue'
+import { defineAsyncComponent } from "vue";
+
+// Lazy loading
+const WSController = defineAsyncComponent(
+  () => import("./WSController.vue")
+);
+
+</script>
+
+<style scoped lang="scss">
+
+.chart {
+  // height: auto;
+  width: 50vw;
+}
+</style>
